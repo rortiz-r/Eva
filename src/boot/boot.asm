cb13344a3cc4d7530dda49995c6d0791216e2fc2
Rafael <rafael@ssh.com>
2025-12-17 16:15:09 -0500

    Basic bootloader

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,5 +1,5 @@
 org 0x7c00
-bit 16
+bits 16
 
 
 start:
@@ -9,4 +9,26 @@ start:
 
 
 print:
-    
+    mov bx, 0
+
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+
+.done:
+    ret
+
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'EVA OS', 0
+
+times 510-($-$$) db 0
+dw 0xAA55
+
