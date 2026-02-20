4216470303ea572f624e0c11d016bb6cfc090c0f
Rafael <rafael@ssh.com>
2026-02-20 14:29:18 -0500

    Reestructuring project, switching to protected mode and adding gdt to bootloader

---
+++ Makefile
@@ -0,0 +1,7 @@
+all:
+	nasm -f bin ./src/boot/boot.asm -o ./bin/boot.bin
+
+
+clean:
+	rm -rf ./bin/boot.bin
+	
--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,41 +1,84 @@
-org 0
+org 0x7c00
 bits 16
 
+; This will give us the offsets of 0x8 and 0x10
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+_start:
+    jmp short start
+    nop
+
+times 33 db 0 ; Create 33 bytes after the short jump.
 
 start:
+    jmp 0: step2
+
+
+step2:
     cli
-    mov ax, 0x7c0
+    mov ax, 0x00
+    mov ds, ax
+    mov es, ax  
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; Enabling interrupts
+
+
+.load_proteced:
+    cli
+    lgdt[gdt_descriptor] ; This load our gdt_descriptor load the size and load the offset and the start of the gdt
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+
+gdt_start:
+
+
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:
+    dw 0xffff; Segment limit first 0-15 bits
+    dw 0; Base first 0-15 bits
+    db 0; Base 16-23 bits
+    db 0x9a ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0
+; offset 0x10
+
+gdt_data:   ; DS, SS, ES, FS, GS
+    dw 0xffff; Segment limit first 0-15 bits
+    dw 0; Base first 0-15 bits
+    db 0; Base 16-23 bits
+    db 0x92 ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+
+[BITS 32] ; Todo el codigo debajo será 32 bits.
+load32: 
+    mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
-    sti
-    mov si, message
-    call print
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-
-print:
-    mov bx, 0
-
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-message: db 'EVA OS', 0
 
 times 510-($-$$) db 0
 dw 0xAA55
+
 
